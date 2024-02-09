4b4afec57edba3862182cdb7e0207169bffa977e
Machai Gratts <machaidgratts@gmail.com>
2024-02-08 21:26:16 -0600

    netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import {useState} from 'react'
-import './App.css'
-import { puppyList } from './data.js'
-
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
-      <div>
-        { 
-   puppies.map((puppy) => {
-     return <p>{puppy.name}{puppy.age}</p>
-   })
+    <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
-      </div>
-    )
-}
-
-export default App
+export default App;
